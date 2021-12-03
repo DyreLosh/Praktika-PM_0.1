d8b007d492944825bbd6e3e57eff99cab06aed0b
Dinar Churakov <dyrelosh@gmail.com>
2021-12-03 19:02:49 +0300

    Add files

---
+++ Задание 4/4.4.WinDataBinding/Praktika/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Praktika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sotrudnikDataSet1 = new Praktika.sotrudnikDataSet();
+            this.сотрудникиTableAdapter1 = new Praktika.sotrudnikDataSetTableAdapters.СотрудникиTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FamtextBox = new System.Windows.Forms.TextBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.SectiontextBox = new System.Windows.Forms.TextBox();
+            this.Previousbutton = new System.Windows.Forms.Button();
+            this.Nextbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sotrudnikDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sotrudnikDataSet1
+            // 
+            this.sotrudnikDataSet1.DataSetName = "sotrudnikDataSet";
+            this.sotrudnikDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // сотрудникиTableAdapter1
+            // 
+            this.сотрудникиTableAdapter1.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отдел";
+            // 
+            // FamtextBox
+            // 
+            this.FamtextBox.Location = new System.Drawing.Point(105, 14);
+            this.FamtextBox.Name = "FamtextBox";
+            this.FamtextBox.Size = new System.Drawing.Size(100, 20);
+            this.FamtextBox.TabIndex = 3;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(105, 52);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox.TabIndex = 4;
+            // 
+            // SectiontextBox
+            // 
+            this.SectiontextBox.Location = new System.Drawing.Point(105, 91);
+            this.SectiontextBox.Name = "SectiontextBox";
+            this.SectiontextBox.Size = new System.Drawing.Size(100, 20);
+            this.SectiontextBox.TabIndex = 5;
+            // 
+            // Previousbutton
+            // 
+            this.Previousbutton.Location = new System.Drawing.Point(269, 33);
+            this.Previousbutton.Name = "Previousbutton";
+            this.Previousbutton.Size = new System.Drawing.Size(75, 23);
+            this.Previousbutton.TabIndex = 6;
+            this.Previousbutton.Text = "Previous";
+            this.Previousbutton.UseVisualStyleBackColor = true;
+            this.Previousbutton.Click += new System.EventHandler(this.Previousbutton_Click);
+            // 
+            // Nextbutton
+            // 
+            this.Nextbutton.Location = new System.Drawing.Point(269, 74);
+            this.Nextbutton.Name = "Nextbutton";
+            this.Nextbutton.Size = new System.Drawing.Size(75, 23);
+            this.Nextbutton.TabIndex = 7;
+            this.Nextbutton.Text = "Next";
+            this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 298);
+            this.Controls.Add(this.Nextbutton);
+            this.Controls.Add(this.Previousbutton);
+            this.Controls.Add(this.SectiontextBox);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.FamtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sotrudnikDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private sotrudnikDataSet sotrudnikDataSet1;
+        private sotrudnikDataSetTableAdapters.СотрудникиTableAdapter сотрудникиTableAdapter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FamtextBox;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.TextBox SectiontextBox;
+        private System.Windows.Forms.Button Previousbutton;
+        private System.Windows.Forms.Button Nextbutton;
+    }
+}
+
---
+++ Задание 4/4.4.WinDataBinding/Praktika/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private BindingSource sotrBindingSourse;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            сотрудникиTableAdapter1.Fill(sotrudnikDataSet1.Сотрудники);
+            sotrBindingSourse = new
+           BindingSource(sotrudnikDataSet1, "Сотрудники");
+            FamtextBox.DataBindings.Add("Text", sotrBindingSourse,
+            "Фамилия");
+            NametextBox.DataBindings.Add("Text",
+           sotrBindingSourse, "Имя");
+            SectiontextBox.DataBindings.Add("Text",
+           sotrBindingSourse, "Отдел");
+        }
+
+        private void Previousbutton_Click(object sender, EventArgs e)
+        {
+            sotrBindingSourse.MovePrevious();
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            sotrBindingSourse.MoveNext();
+        }
+    }
+}
